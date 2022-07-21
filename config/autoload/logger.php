e9f1bb43a5f40ecd466bee80de51b56c3d0cd594
李铭昕 <715557344@qq.com>
2022-07-21 19:43:06 +0800

    将日志输出到终端

--- config/autoload/logger.php
+++ config/autoload/logger.php
@@ -16,7 +16,7 @@ return [
         'handler' => [
             'class' => Monolog\Handler\StreamHandler::class,
             'constructor' => [
-                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
+                'stream' => 'php://output',
                 'level' => Monolog\Logger::INFO,
             ],
         ],
